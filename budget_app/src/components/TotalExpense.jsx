791aea8602e639d585c59a89d94c2e723e53a929
abdulkareem <abdulkareembello93@gmail.com>
2024-02-27 22:37:07 +0100

    completed part of the UI

---
+++ budget_app/src/components/TotalExpense.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalExpense = () => {
+  return (
+    <div className="alert alert-primary">
+      <span>Spent so far: $1200</span>
+    </div>
+  );
+};
+
+export default TotalExpense;
