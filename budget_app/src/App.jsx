10a0ea2945df18a736b76217df94c33190909eca
abdulkareem <abdulkareembello93@gmail.com>
2024-02-28 21:46:27 +0100

    completed the ui

--- budget_app/src/App.jsx
+++ budget_app/src/App.jsx
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget'
 import Remaining from './components/Remaining'
 import TotalExpense from './components/TotalExpense';
+import ExpenseList from './components/ExpenseList'
+import AddExpenseForm from './components/AddExpenseForm';
+
 
 const App = () => {
   return (
@@ -17,6 +20,18 @@ const App = () => {
         </div>
         <div className="col-sm">
           <TotalExpense />
+        </div>
+      </div>
+      <h3 className="mt-3">Expenses</h3>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <ExpenseList />
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className="mt-3">
+        <div className="col-sm">
+          <AddExpenseForm/>
         </div>
       </div>
     </div>
---
+++ budget_app/src/components/AddExpenseForm.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const AddExpenseForm = () => {
+  return (
+    <form>
+      <div className="row">
+        <div className="col-sm">
+          <label htmlFor="name">
+            Name
+          </label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+          ></input>
+        </div>
+        <div className="col-sm">
+          <label htmlFor="cost">Cost</label>
+          <input
+            type="text"
+            required="required"
+            className="form-control"
+            id="cost"
+          />
+        </div>
+        <div className="col-sm">
+            <button type="submit" className="btn btn-primary mt-3">
+                save
+            </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default AddExpenseForm;
