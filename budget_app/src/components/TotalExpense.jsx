b2a4f02a3b7f59380216b64cf16436e2b6e5bfcc
abdulkareem <abdulkareembello93@gmail.com>
2024-02-29 17:08:59 +0100

    added edit button to the budget

--- budget_app/src/components/TotalExpense.jsx
+++ budget_app/src/components/TotalExpense.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const TotalExpense = () => {
+  const {expenses} = useContext(AppContext)
+
+  const AllExpenses = expenses.reduce((total, item)=> {
+    return (total = total + item.cost)
+  }, 0)
+
+  
+
   return (
     <div className="alert alert-primary">
-      <span>Spent so far: $1200</span>
+      <span>Spent so far: ${AllExpenses}</span>
     </div>
   );
 };
